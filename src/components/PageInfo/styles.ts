b45ecd5fc1a6232e286ca76cbe196887e3d8199a
Caio <whoiscaio@protonmail.com>
2022-02-07 13:33:25 -0300

    feat: general overview styles added

--- src/components/PageInfo/styles.ts
+++ src/components/PageInfo/styles.ts
@@ -7,7 +7,9 @@ const AppInfoContainer = styled.div`
   flex-direction: column;
   justify-content: center;
 
-  h1, p {
+  pointer-events: none;
+
+  h1, > p {
     color: ${({ theme }) => theme.colors.text};
     padding-right: 8rem;
   }
@@ -25,6 +27,43 @@ const AppInfoContainer = styled.div`
 
   a {
     color: ${({ theme }) => theme.colors.contrast};
+  }
+
+  .all-country-overview {
+    max-width: 500px;
+
+    background: ${({ theme }) => theme.colors.contrast};
+    border-radius: ${({ theme }) => theme.measures.borderRadius};
+
+    display: flex;
+    justify-content: space-around;
+
+    margin-top: 2rem;
+    padding: 1rem 0;
+
+    p, span {
+      color: #F6EDD9;
+    }
+
+    span {
+      font-size: 1.4rem;
+      margin-bottom: .6rem;
+    }
+
+    div {
+      flex: 1;
+      
+      
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      &.deaths {
+        border-right: 1px solid ${({ theme }) => theme.colors.background};
+        border-left: 1px solid ${({ theme }) => theme.colors.background};
+      }
+    }
   }
 
   @media (max-width: 999px) {
